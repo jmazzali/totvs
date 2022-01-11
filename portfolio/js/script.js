ebd5a52d875a9330380ccc3ba89250d015c9744d
Joao Mazzali <joaovitormazzali@gmail.com>
2022-01-10 21:30:22 -0300

    função para trocar cor do plano de fundo e mudança na declaração da função de incluir despesa

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -9,15 +9,43 @@ menuMobile.addEventListener("click", () => {
     show = !show
 })
 
+/**/
+var body = document.querySelector("body")
+var sectionLevel = document.getElementById("level")
+var classFont = document.getElementsByClassName("fontdefault")
+
+var themeA = 0
+
+function changeBC(){
+    if(themeA === 0){
+        body.style.backgroundColor = 'black';
+        sectionLevel.style.backgroundColor = 'rgb(129 129 129 / 37%)';
+
+        for (i = 0; i < classFont.length; i++) {
+            classFont[i].style.color = "white"
+        }
+        themeA = 1
+    } else{
+        body.style.backgroundColor = 'white';
+        sectionLevel.style.backgroundColor = 'rgba(223, 223, 223, 0.493)';
+
+        for (i = 0; i < classFont.length; i++) {
+            classFont[i].style.color="rgb(3, 0, 44)"
+        }
+        themeA = 0
+    }
+}
+
+
 /*BOTÃO INCLUIR DA PAGINA FORMULÁRIO*/
 const incluir = document.getElementById("btn-incluir")
 
-incluir.addEventListener("click", () =>{
+function incluirDiv(){
     var node = document.createElement("div");
-    node.classList.add("obj", "flex-row-center")
+    node.classList.add("obj", "flex-row-center");
 
     var textnode = document.createTextNode("");
     node.appendChild(textnode);
 
     document.getElementById("infos-form").appendChild(node);
-})
+}
